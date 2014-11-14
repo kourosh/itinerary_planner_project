5d6243d52b449016f2ea21548a2b8049c4d007a4
Kourosh Karimkhany <kouroshk@gmail.com>
2014-11-14 15:56:23 -0800

    Create routes through rake routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ItineraryPlanner::Application.routes.draw do
+
+
+  resources :itineraries do
+    resources :waypoints
+  end
+
+  resources :travelers
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
